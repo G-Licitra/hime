417c4b3bd7be8f6b600ba878c86c21aa217d7dd3
Giovanni Licitra <giovanni@Giovannis-MacBook-Pro.local>
2021-05-05 21:57:18 +0200

    add parser_formula

--- romeo/romeo.py
+++ romeo/romeo.py
@@ -1,9 +1,12 @@
+from __future__ import division
+
 import logging
 
 import numpy as np
 import pandas as pd
 from sklearn.exceptions import NotFittedError
 from sklearn.linear_model import LinearRegression
+
 
 logger = logging.getLogger(__name__)
 
@@ -23,5 +26,5 @@ class ROMEO:
 
         if self.trained_model is None:
             raise NotFittedError(
-                "The instance is not fitted yet. Call 'fit' with appropriate arguments before using this method"
+                """The instance is not fitted yet. Call 'fit' before using this method"""
             )
--- romeo/utils.py
+++ romeo/utils.py
@@ -1,27 +1,49 @@
-# Import seaborn
 import re
 
-import seaborn as sns
 
-# Load an example dataset
-data = sns.load_dataset("iris")
+def formula_parser(formula: str):
+    r"""Extrapolate the target, fixed and random effect terms.
 
+     Parameters
+     ----------
+     formula: str
+         Dataframe with variables in columns.
 
-formula = "species ~ sepal_length + sepal_width + petal_length + petal_width + (1 | type) + (1|store)"
+     Returns
+     -------
+     dict : dict
+         dictionary containing info necessary to construct problem formulation
 
-formula_fmt = [x.replace(" ", "") for x in formula.split("~")]
+    Examples
+     ----------
+     >>> formula = "y ~  x1 + x2 + x3 + (1 | c1) + (1 | c2)"
+     >>> dict_formula = formula_parser(formula=formula)
 
-lhs = formula_fmt[0]  # left hand side equation
-rhs = formula_fmt[1]  # right hand side equation
+    """
 
-fixed_effect = []
-random_effect = []
+    # split between DV and fixed + random effect
+    formula_fmt = [x.replace(" ", "") for x in formula.split("~")]
+    lhs = formula_fmt[0]  # left hand side equation
+    rhs = formula_fmt[1]  # right hand side equation
 
-for elem in rhs.split("+"):
-    if re.compile("\((.+)\)").search(elem) is not None:
-        random_effect.append(re.match("\((.+)\)", elem).group(0))
-    else:
-        fixed_effect.append(elem)
+    # from rhs discern between fixed and random effect
+    fixed_effect = []
+    random_effect = []
 
-print(fixed_effect)
-print(random_effect)
+    for elem in rhs.split("+"):
+        # check is elem has is contained in parenthesis "()"
+        if re.compile(r"\((.+)\)").search(elem) is not None:
+            # append random effect element in the form "(1|group)"
+            random_effect.append(re.match(r"\((.+)\)", elem).group(0))
+        else:
+            # append fixed effect element in the form
+            fixed_effect.append(elem)
+
+    clusters = [re.match(r"^.*\|(.*)\).*$", x).group(1) for x in random_effect]
+
+    return {
+        "target": lhs,
+        "fixed_effect": fixed_effect,
+        "random_effect": random_effect,
+        "clusters": clusters,
+    }
