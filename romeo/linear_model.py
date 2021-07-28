c1512c4683d16a21f0e6bb606c38266bd88c5693
James Twose <jamestwose@Mac-mini.home>
2021-07-28 17:08:40 +0200

    Adding LassoRegression and RidgeRegression classes to romeo.linear_model, these are both variants on LinearRegression and as such use this as the BaseEstimator. The only difference is that the fit() method is defined slightly differently

--- romeo/linear_model.py
+++ romeo/linear_model.py
@@ -8,6 +8,7 @@ from scipy import stats
 from sklearn.base import BaseEstimator#, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
+
 class LinearRegression(BaseEstimator):
 
     def __init__(self, fit_intercept = True,
@@ -20,6 +21,7 @@ class LinearRegression(BaseEstimator):
         self.normalize = normalize
         self.copy_X = copy_X
         self.positive = positive
+        self.is_fitted_ = False
 
     def conf_int(self, alpha=.05, cols=None):
         """
@@ -359,6 +361,224 @@ class LinearRegression(BaseEstimator):
         return y_pred
 
 
+class LassoRegression(LinearRegression):
+
+    def fit(self, X, y, sample_weight=None, verbose=True, alpha=1):
+        """docstring"""
+
+        # TODO: self._preprocess_data
+        # X, y, X_offset, y_offset, X_scale = self._preprocess_data(
+        #    X, y, fit_intercept=self.fit_intercept, normalize=self.normalize,
+        #    copy=self.copy_X, sample_weight=sample_weight,
+        #    return_mean=True)
+
+        if isinstance(X, pd.DataFrame):
+            column_names = X.columns.tolist()
+        else:
+            column_names = list(range(X.shape[1]))
+
+        self.predictors = X
+        self.target = y
+
+        if self.copy_X:
+            X = X.copy()
+
+        X, y = check_X_y(X, y, accept_sparse=True)
+
+        if self.fit_intercept:
+            # # add 1 col using broadcasting
+            # X["intercept"] = 1
+            # # move intercept col at the first column
+            # cols = X.columns.tolist()
+            # cols = cols[-1:] + cols[:-1]
+            # X = X[cols]
+            X = np.c_[np.ones(X.shape[0]), X]
+
+        (N, ntheta) = X.shape
+        theta = ca.SX.sym("theta", ntheta)
+
+        # create residual
+        e = y - ca.mtimes(X, theta)
+
+        # create optimization problem (x: optimization parameter, f: cost function)
+        nlp = {"x": theta, "f": 0.5*ca.dot(e, e) + 0.5*alpha*ca.sum1(ca.fabs(theta))}
+
+        # solve opt
+        solver = ca.nlpsol("ols", "ipopt", nlp)
+        if verbose:
+            sol = solver(x0=np.zeros(ntheta))
+        else:
+            with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+                sol = solver(x0=np.zeros(ntheta))
+
+        self.is_fitted_ = True
+
+        if self.fit_intercept:
+            self.intercept_ = sol["x"].full().ravel()[0]
+            self.coef_ = sol["x"].full().ravel()[1:]
+            summary_index = ["intercept"] + column_names
+        else:
+            self.intercept_ = 0
+            self.coef_ = sol["x"].full().ravel()
+            summary_index = column_names
+
+        self.params = sol["x"].full().ravel()
+
+        tmp_x = self.intercept_ * X
+        cov_mat = np.linalg.inv(np.matmul(tmp_x.transpose(1, 0), tmp_x))
+        self.bse = np.sqrt(np.diag(cov_mat))
+        self.tvalues = sol["x"].full().ravel() / self.bse
+        self.df_resid = X.shape[0] - X.shape[1]
+        self.df_model = X.shape[1] - 1
+        self.pvalues = stats.t.sf(np.abs(self.tvalues), self.df_resid) * 2
+        self.summary_ = (pd.DataFrame(data={"coef": sol["x"].full().ravel(),
+                                            "std_err": self.bse,
+                                            "t": self.tvalues,
+                                            "P>|t|": self.pvalues},
+                                      index=summary_index)
+                         .join(pd.DataFrame(self.conf_int(), columns=["[0.025", "0.975]"],
+                                            index=summary_index))
+                         )
+
+        self.resid = self.resid()
+        self.ssr = self.ssr()
+        self.uncentered_tss = self.uncentered_tss()
+        self.rsquared = self.rsquared()
+        self.nobs = X.shape[0]
+        self.rsquared_adj = self.rsquared_adj()
+        self.ess = self.ess()
+        self.mse_model = self.mse_model()
+        self.mse_resid = self.mse_resid()
+        self.mse_total = self.mse_total()
+        self.fvalue = self.fvalue()
+        self.f_pvalue = self.f_pvalue()
+        self.llf = self.loglike()
+        self.aic = self.aic()
+        self.bic = self.bic()
+
+        self.fit_evaluation_ = (pd.DataFrame(data={
+            "r_squared": self.rsquared,
+            "r_squared_adj": self.rsquared_adj,
+            "f_statistic": self.fvalue,
+            "f_statistic_pvalue": self.f_pvalue,
+            "log_likelihood": self.llf,
+            "AIC": self.aic,
+            "BIC": self.bic,
+        },
+            index=["model_evaluation"])
+        )
+
+        return self
+
+
+class RidgeRegression(LinearRegression):
+
+    def fit(self, X, y, sample_weight=None, verbose=True, alpha=1):
+        """docstring"""
+
+        # TODO: self._preprocess_data
+        # X, y, X_offset, y_offset, X_scale = self._preprocess_data(
+        #    X, y, fit_intercept=self.fit_intercept, normalize=self.normalize,
+        #    copy=self.copy_X, sample_weight=sample_weight,
+        #    return_mean=True)
+
+        if isinstance(X, pd.DataFrame):
+            column_names = X.columns.tolist()
+        else:
+            column_names = list(range(X.shape[1]))
+
+        self.predictors = X
+        self.target = y
+
+        if self.copy_X:
+            X = X.copy()
+
+        X, y = check_X_y(X, y, accept_sparse=True)
+
+        if self.fit_intercept:
+            # # add 1 col using broadcasting
+            # X["intercept"] = 1
+            # # move intercept col at the first column
+            # cols = X.columns.tolist()
+            # cols = cols[-1:] + cols[:-1]
+            # X = X[cols]
+            X = np.c_[np.ones(X.shape[0]), X]
+
+        (N, ntheta) = X.shape
+        theta = ca.SX.sym("theta", ntheta)
+
+        # create residual
+        e = y - ca.mtimes(X, theta)
+
+        # create optimization problem (x: optimization parameter, f: cost function)
+        nlp = {"x": theta, "f": 0.5*ca.dot(e, e) + 0.5*alpha*ca.dot(theta, theta)}
+
+        # solve opt
+        solver = ca.nlpsol("ols", "ipopt", nlp)
+        if verbose:
+            sol = solver(x0=np.zeros(ntheta))
+        else:
+            with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+                sol = solver(x0=np.zeros(ntheta))
+
+        self.is_fitted_ = True
+
+        if self.fit_intercept:
+            self.intercept_ = sol["x"].full().ravel()[0]
+            self.coef_ = sol["x"].full().ravel()[1:]
+            summary_index = ["intercept"] + column_names
+        else:
+            self.intercept_ = 0
+            self.coef_ = sol["x"].full().ravel()
+            summary_index = column_names
+
+        self.params = sol["x"].full().ravel()
+
+        tmp_x = self.intercept_ * X
+        cov_mat = np.linalg.inv(np.matmul(tmp_x.transpose(1, 0), tmp_x))
+        self.bse = np.sqrt(np.diag(cov_mat))
+        self.tvalues = sol["x"].full().ravel() / self.bse
+        self.df_resid = X.shape[0] - X.shape[1]
+        self.df_model = X.shape[1] - 1
+        self.pvalues = stats.t.sf(np.abs(self.tvalues), self.df_resid) * 2
+        self.summary_ = (pd.DataFrame(data={"coef": sol["x"].full().ravel(),
+                                            "std_err": self.bse,
+                                            "t": self.tvalues,
+                                            "P>|t|": self.pvalues},
+                                      index=summary_index)
+                         .join(pd.DataFrame(self.conf_int(), columns=["[0.025", "0.975]"],
+                                            index=summary_index))
+                         )
+
+        self.resid = self.resid()
+        self.ssr = self.ssr()
+        self.uncentered_tss = self.uncentered_tss()
+        self.rsquared = self.rsquared()
+        self.nobs = X.shape[0]
+        self.rsquared_adj = self.rsquared_adj()
+        self.ess = self.ess()
+        self.mse_model = self.mse_model()
+        self.mse_resid = self.mse_resid()
+        self.mse_total = self.mse_total()
+        self.fvalue = self.fvalue()
+        self.f_pvalue = self.f_pvalue()
+        self.llf = self.loglike()
+        self.aic = self.aic()
+        self.bic = self.bic()
+
+        self.fit_evaluation_ = (pd.DataFrame(data={
+            "r_squared": self.rsquared,
+            "r_squared_adj": self.rsquared_adj,
+            "f_statistic": self.fvalue,
+            "f_statistic_pvalue": self.f_pvalue,
+            "log_likelihood": self.llf,
+            "AIC": self.aic,
+            "BIC": self.bic,
+        },
+            index=["model_evaluation"])
+        )
+
+        return self
 
 # executable for stand-alone program
 if __name__ == "__main__":
