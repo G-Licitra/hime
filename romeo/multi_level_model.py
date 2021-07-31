ec15603dd68e7f6308433b6895e49385662c90f0
James Twose <jamestwose@Mac-mini.home>
2021-07-31 09:06:58 +0200

    Getting the very first, very rough version of the LinearMixedRegression class in the multi_level_model scipt in ROMEO, it currently only has a fit method, and only works for models specified like the example in the 'notebook/romeo_ALL_types_LMM.ipynb' script, next steps are to make it more flexible to other model specifications and to add the predict method, plus a bunch of other smaller TODOs

--- romeo/multi_level_model.py
+++ romeo/multi_level_model.py
@@ -6,8 +6,10 @@ import contextlib
 from scipy import stats
 
 # from sklearn.base import BaseEstimator#, ClassifierMixin, TransformerMixin
-from linear_model import LinearRegression
+from .linear_model import LinearRegression
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+
+flatten = lambda l: [item for sublist in l for item in sublist]
 
 class LinearMixedRegression(LinearRegression):
 
@@ -22,7 +24,7 @@ class LinearMixedRegression(LinearRegression):
         self.copy_X = copy_X
         self.positive = positive
 
-    def fit(self, X, y, sample_weight=None, verbose=True):
+    def fit(self, X, y, sample_weight=None, verbose=True, kwargs=dict()):
         """docstring"""
 
         # TODO: change the fit method to match the LMM framework
@@ -31,6 +33,22 @@ class LinearMixedRegression(LinearRegression):
             column_names = X.columns.tolist()
         else:
             column_names = list(range(X.shape[1]))
+
+        grouping_var = kwargs["grouping_var"]
+        self.u = X[grouping_var].sort_values().unique()
+
+        z_list = list()
+        for cluster_group in self.u:
+            tmp = X.loc[X[grouping_var] == cluster_group, grouping_var]
+            z_list.append(
+                np.pad(tmp, (tmp.index.tolist()[0], X[grouping_var].shape[0] - tmp.index.tolist()[-1] + 1)))
+
+        possible_Z_df = pd.DataFrame(np.vstack(z_list).T)
+        binary_df = np.where(possible_Z_df > 0, 1, possible_Z_df)
+        self.Z = binary_df[0: X.shape[0]]
+
+        fixed_effect = kwargs["fixed_effect"]
+        X = X[[fixed_effect]]
 
         self.predictors = X
         self.target = y
@@ -49,29 +67,34 @@ class LinearMixedRegression(LinearRegression):
             # X = X[cols]
             X = np.c_[np.ones(X.shape[0]), X]
 
-        (N, ntheta) = X.shape
-        theta = ca.SX.sym("theta", ntheta)
+        ntheta_fixed = X.shape[1]  # (time,)
+        ntheta_random = len(self.u)  # (amount of pigs groups)
+        theta_fixed = ca.SX.sym("theta_fixed", ntheta_fixed)
+        theta_random = ca.SX.sym("theta_random", ntheta_random)
+
+        model_lmm = ca.mtimes(X, theta_fixed) + ca.mtimes(self.Z, theta_random)
 
         # create residual
-        e = y - ca.mtimes(X, theta)
+        e = y.reshape(-1, 1) - model_lmm
 
         # create optimization problem (x: optimization parameter, f: cost function)
-        nlp = {"x": theta, "f": 0.5 * ca.dot(e, e)}
+        nlp = {"x": ca.vertcat(theta_fixed, theta_random), "f": 0.5 * ca.dot(e, e), }
 
         # solve opt
         solver = ca.nlpsol("ols", "ipopt", nlp)
+
         if verbose:
-            sol = solver(x0=np.zeros(ntheta))
+            sol = solver(x0=np.zeros(ntheta_fixed + ntheta_random))
         else:
             with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
-                sol = solver(x0=np.zeros(ntheta))
+                sol = solver(x0=np.zeros(ntheta_fixed + ntheta_random))
 
         self.is_fitted_ = True
 
         if self.fit_intercept:
             self.intercept_ = sol["x"].full().ravel()[0]
             self.coef_ = sol["x"].full().ravel()[1:]
-            summary_index = ["intercept"] + column_names
+            summary_index = ["intercept"] + column_names[:-1] + self.u.tolist()
         else:
             self.intercept_ = 0
             self.coef_ = sol["x"].full().ravel()
@@ -79,49 +102,64 @@ class LinearMixedRegression(LinearRegression):
 
         self.params = sol["x"].full().ravel()
 
-        tmp_x = self.intercept_ * X
-        cov_mat = np.linalg.inv(np.matmul(tmp_x.transpose(1, 0), tmp_x))
-        self.bse = np.sqrt(np.diag(cov_mat))
-        self.tvalues = sol["x"].full().ravel() / self.bse
-        self.df_resid = X.shape[0] - X.shape[1]
-        self.df_model = X.shape[1] - 1
-        self.pvalues = stats.t.sf(np.abs(self.tvalues), self.df_resid) * 2
+        # TODO: Take the intercept into account when calculating the fitted values
+        theta_est = sol["x"]
+        results = flatten(theta_est.toarray().tolist())
+        Zu = np.dot(self.Z, results[2:])
+        XB = X * results[1]
+        self.fitted_values = pd.DataFrame(XB)[1].add(pd.Series(Zu), axis=0)
+
+        # tmp_x = self.intercept_ * X
+        # cov_mat = np.linalg.inv(np.matmul(tmp_x.transpose(1, 0), tmp_x))
+        # self.bse = np.sqrt(np.diag(cov_mat))
+        # self.tvalues = sol["x"].full().ravel() / self.bse
+        # self.df_resid = X.shape[0] - X.shape[1]
+        # self.df_model = X.shape[1] - 1
+        # self.pvalues = stats.t.sf(np.abs(self.tvalues), self.df_resid) * 2
+        # self.summary_ = (pd.DataFrame(data={"coef": sol["x"].full().ravel(),
+        #                                     "std_err": self.bse,
+        #                                     "t": self.tvalues,
+        #                                     "P>|t|": self.pvalues},
+        #                               index=summary_index)
+        #                  .join(pd.DataFrame(self.conf_int(), columns=["[0.025", "0.975]"],
+        #                                     index=summary_index))
+        #                  )
         self.summary_ = (pd.DataFrame(data={"coef": sol["x"].full().ravel(),
-                                            "std_err": self.bse,
-                                            "t": self.tvalues,
-                                            "P>|t|": self.pvalues},
-                                      index=summary_index)
-                         .join(pd.DataFrame(self.conf_int(), columns=["[0.025", "0.975]"],
-                                            index=summary_index))
+                                            # "std_err": self.bse,
+                                            # "t": self.tvalues,
+                                            # "P>|t|": self.pvalues
+                                            },
+                                      index=summary_index
+                                      )
                          )
-
-        self.resid = self.resid()
-        self.ssr = self.ssr()
-        self.uncentered_tss = self.uncentered_tss()
-        self.rsquared = self.rsquared()
-        self.nobs = X.shape[0]
-        self.rsquared_adj = self.rsquared_adj()
-        self.ess = self.ess()
-        self.mse_model = self.mse_model()
-        self.mse_resid = self.mse_resid()
-        self.mse_total = self.mse_total()
-        self.fvalue = self.fvalue()
-        self.f_pvalue = self.f_pvalue()
-        self.llf = self.loglike()
-        self.aic = self.aic()
-        self.bic = self.bic()
-
-        self.fit_evaluation_ = (pd.DataFrame(data={
-            "r_squared": self.rsquared,
-            "r_squared_adj": self.rsquared_adj,
-            "f_statistic": self.fvalue,
-            "f_statistic_pvalue": self.f_pvalue,
-            "log_likelihood": self.llf,
-            "AIC": self.aic,
-            "BIC": self.bic,
-        },
-            index=["model_evaluation"])
-                                )
+        #
+        # self.resid = self.resid()
+        # self.ssr = self.ssr()
+        # self.uncentered_tss = self.uncentered_tss()
+        # self.rsquared = self.rsquared()
+        # self.nobs = X.shape[0]
+        # self.rsquared_adj = self.rsquared_adj()
+        # self.ess = self.ess()
+        # self.mse_model = self.mse_model()
+        # self.mse_resid = self.mse_resid()
+        # self.mse_total = self.mse_total()
+        # self.fvalue = self.fvalue()
+        # self.f_pvalue = self.f_pvalue()
+        # self.llf = self.loglike()
+        # self.aic = self.aic()
+        # self.bic = self.bic()
+        #
+        # self.fit_evaluation_ = (pd.DataFrame(data={
+        #     "r_squared": self.rsquared,
+        #     "r_squared_adj": self.rsquared_adj,
+        #     "f_statistic": self.fvalue,
+        #     "f_statistic_pvalue": self.f_pvalue,
+        #     "log_likelihood": self.llf,
+        #     "AIC": self.aic,
+        #     "BIC": self.bic,
+        # },
+        #     index=["model_evaluation"])
+        #                         )
 
         return self
 
