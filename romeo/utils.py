Adding the LogisticRegression Class and LogisticMixedModel - need to update the fittedvalues formula and predict methods as they are currently incorrect

romeo/utils.py
@@ -1,5 +1,5 @@
 import re
-
+import casadi as ca
 
 def formula_parser(formula: str):
     r"""Extrapolate the target, fixed and random effect terms.
@@ -47,3 +47,17 @@ def formula_parser(formula: str):
         "random_effect": random_effect,
         "clusters": clusters,
     }
+
+def sigmoid(z):
+    # Note it might be necessary to replace the np.exp with casadi version
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+    s = 1 / (1 + ca.exp(-z))
+    return s