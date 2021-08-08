697f9818c841875edf2848383ba4913fcc3d4e9b
G-Licitra <gianni.licitra7@gmail.com>
2021-08-09 00:26:07 +0200

    add extra benchmark

--- tests/baseline/benchmark_linear_models.py
+++ tests/baseline/benchmark_linear_models.py
@@ -1,6 +1,10 @@
+# add extra case from here
+# https://www.statsmodels.org/stable/examples/notebooks/generated/ols.html#OLS-with-dummy-variables
+
+
 import pandas as pd
 
-# Spector.csv =====================================================
+# Spector OLS =====================================================
 df_summary = pd.Series(data={"Model": "OLS",
 "Dep. Variable": "GRADE",
 "R-squared (uncentered)": 0.505,
@@ -35,7 +39,7 @@ df_residual = pd.Series(data={"Omnibus": 3.670,
                                 "Cond. No.": 45.6
                               })
 
-# case with intercept -------------------------------------------
+# Spector OLS (intercept) -------------------------------------------
 
 df_summary = pd.Series(data={"Model": "OLS",
 "Dep. Variable": "GRADE",
@@ -54,10 +58,10 @@ df_summary = pd.Series(data={"Model": "OLS",
 })
 
 df_coef = pd.DataFrame(data= [[0.4639, 0.162, 2.864, 0.008, 0.132, 0.796],
-                    [0.0105, 0.019 , 0.539, 0.594, -0.029, 0.050],
-                    [0.3786, 0.139, 2.720, 0.011, 0.093, 0.664],
-                    [-1.4980, 0.524, -2.859, 0.008, -2.571, -0.425],
-                               ],
+                              [0.0105, 0.019 , 0.539, 0.594, -0.029, 0.050],
+                              [0.3786, 0.139, 2.720, 0.011, 0.093, 0.664],
+                              [-1.4980, 0.524, -2.859, 0.008, -2.571, -0.425],
+                            ],
              columns=["coef", "std err", "t",  "P>|t|", "[0.025", "0.975]"],
              index= ["GPA", "TUCE" , "PSI", "intercept"]
 )
@@ -72,4 +76,18 @@ df_residual = pd.Series(data={"Omnibus": 0.176,
                                 "Cond. No.": 176
                               })
 
+
+
+# Spector OLS | intercept | X normalized (sklearn) -------------------------------------
+# y_fit_normalized = y_fittevalues
+# df_summary = df_summary without normalization
+# df_residual  = df_residual  without normalization
+
+df_coef = pd.DataFrame(data= [[0.2131, 0.074, 2.864, 0.008,  0.061, 0.365],
+                              [0.0403, 0.075, 0.539, 0.594, -0.113, 0.194],
+                              [0.1878, 0.069, 2.720, 0.011,  0.046, 0.329],
+                              [0.3438, 0.069, 5.011, 0.000,  0.203, 0.484],
+                            ],
+             columns=["coef", "std err", "t",  "P>|t|", "[0.025", "0.975]"],
+             index= ["GPA", "TUCE" , "PSI", "intercept"])
 
